5344e0261915e460782945757da82371eae25a4f
Christoph Guttandin <chrisguttandin@media-codings.com>
2015-10-26 00:03:19 +0100

    export S3 client library

--- src/module.js
+++ src/module.js
@@ -2,8 +2,11 @@
 
 var di = require('di'),
     DynamoDbClientFactory = require('./factories/dynamo-db.js').DynamoDbClientFactory,
-    injector;
+    injector,
+    S3ClientFactory = require('./factories/s3.js').S3ClientFactory;
 
 injector = new di.Injector();
 
 module.exports.dynamoDbClientFactory = injector.get(DynamoDbClientFactory);
+
+module.exports.s3ClientFactory = injector.get(S3ClientFactory);
