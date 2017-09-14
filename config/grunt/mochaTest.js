a20f0ada09915771fb9a1acead0e024a30703b75
chrisguttandin <chrisguttandin@media-codings.com>
2017-09-15 01:07:36 +0200

    update test task

--- config/grunt/mochaTest.js
+++ config/grunt/mochaTest.js
@@ -1,6 +1,7 @@
 const babelOptions = require('../babel/test.json');
 const babelRegister = require('babel-register');
 const chai = require('chai');
+const fs = require('fs');
 const sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
@@ -11,12 +12,25 @@ module.exports = {
             bail: true,
             clearRequireCache: true,
             require: [
-                function () {
-                    babelRegister(babelOptions);
+                () => {
+                    const compiler = require.extensions['.ts'];
+
+                    require.extensions['.ts'] = function (mdl, filename) {
+                        if (!filename.includes('node_modules') && filename.includes('src/')) {
+                            const buildFilename = filename
+                                .replace('src/', 'build/node/')
+                                .slice(0, -3) + '.js';
+
+                            mdl._compile(fs.readFileSync(buildFilename, 'utf8'), buildFilename);
+                        }
+
+                        if (compiler) {
+                            return compiler(mdl, filename);
+                        }
+                    };
                 },
-                function () {
-                    global.expect = chai.expect;
-                }
+                () => babelRegister(babelOptions),
+                () => global.expect = chai.expect
             ]
         },
         src: [
