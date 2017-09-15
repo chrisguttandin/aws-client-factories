3d028cb83f48975b4f4ac898b0dd800d3153f1cc
chrisguttandin <chrisguttandin@media-codings.com>
2017-09-15 04:16:29 +0200

    explicitly define exported values with their interfaces

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,9 @@
+import { IDynamoDbClientFactory, IS3ClientFactory } from './interfaces';
 import { DynamoDbClientFactory } from './factories/dynamo-db';
 import { S3ClientFactory } from './factories/s3';
 
 export * from './interfaces';
 
-export const dynamoDbClientFactory = new DynamoDbClientFactory();
+export const dynamoDbClientFactory: IDynamoDbClientFactory = new DynamoDbClientFactory();
 
-export const s3ClientFactory = new S3ClientFactory();
+export const s3ClientFactory: IS3ClientFactory = new S3ClientFactory();
