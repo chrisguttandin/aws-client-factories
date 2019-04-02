8a01357dbe7770b8dc80f5ca92b8606c450fac24
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-04-02 11:32:58 +0200

    style: disable rule which reports false positives

--- src/interfaces/s3-client-factory.ts
+++ src/interfaces/s3-client-factory.ts
@@ -1,7 +1,8 @@
 import { S3 } from 'aws-sdk';
 import { TS3Client } from '../types';
 
-export interface IS3ClientFactory {
+// @todo tslint v5.15.0 does report the next line as an error of the interface-name rule.
+export interface IS3ClientFactory { // tslint:disable-line:interface-name
 
     create (options?: S3.ClientConfiguration): TS3Client;
 
