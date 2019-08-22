64ebe1bedee2a690dc3147aeb3f2eb457890114e
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-22 17:51:50 +0200

    build(config): update linting tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,14 +1,24 @@
-module.exports = {
-    'build': {
-        cmd: 'tsc -p src/tsconfig.json'
-    },
-    'lint': {
-        cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
-    },
-    'test-expectation': {
-        cmd: 'mocha --bail --recursive --require config/mocha/config-expectation.js test/expectation'
-    },
-    'test-unit': {
-        cmd: 'mocha --bail --recursive --require config/mocha/config-unit.js test/unit'
-    }
+module.exports = (grunt) => {
+    const fix = (grunt.option('fix') === true);
+
+    return {
+        'build': {
+            cmd: 'tsc -p src/tsconfig.json'
+        },
+        'lint-config': {
+            cmd: `eslint --config config/eslint/config.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives *.js config/**/*.js`
+        },
+        'lint-src': {
+            cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
+        },
+        'lint-test': {
+            cmd: `eslint --config config/eslint/test.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives test/**/*.js`
+        },
+        'test-expectation': {
+            cmd: 'mocha --bail --recursive --require config/mocha/config-expectation.js test/expectation'
+        },
+        'test-unit': {
+            cmd: 'mocha --bail --recursive --require config/mocha/config-unit.js test/unit'
+        }
+    };
 };
