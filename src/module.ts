155a4e52d422882b3a8ecc1d5ca9aad2dad5e066
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-14 17:42:06 +0100

    build(config): update tsconfig file

--- src/module.ts
+++ src/module.ts
@@ -2,8 +2,12 @@ import { DynamoDbClientFactory } from './factories/dynamo-db';
 import { S3ClientFactory } from './factories/s3';
 import { IDynamoDbClientFactory, IS3ClientFactory } from './interfaces';
 
-export * from './interfaces';
-export * from './types';
+/*
+ * @todo Explicitly referencing the barrel file seems to be necessary when enabling the
+ * isolatedModules compiler option.
+ */
+export * from './interfaces/index';
+export * from './types/index';
 
 export const dynamoDbClientFactory: IDynamoDbClientFactory = new DynamoDbClientFactory();
 
