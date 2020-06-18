f776b76c338da3e73793e7cf1e707eeda08b8703
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-19 00:20:30 +0200

    style: remove outdated tslint comment

--- src/interfaces/s3-client-factory.ts
+++ src/interfaces/s3-client-factory.ts
@@ -1,9 +1,6 @@
 import { S3 } from 'aws-sdk';
 import { TS3Client } from '../types';
 
-// @todo tslint v5.15.0 does report the next line as an error of the interface-name rule.
 export interface IS3ClientFactory {
-    // tslint:disable-line:interface-name
-
     create(options?: S3.ClientConfiguration): TS3Client;
 }
